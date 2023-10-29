c8f0d1361bdbf6732a0908a21934373ab5999f56
Tyler Jennings <tylerjennings2@outlook.com>
2023-10-29 09:36:44 -0400

    Added account button (wip)

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import Logo from "./mainTitle";
 import NavLink from "./navlink";
 import LoginButton from "./login";
-
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Navbar() {
+  const session = useSession();
+  const isLoggedIn = true;
+
   return (
     <nav className="bg-blue-700">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -20,6 +26,23 @@ function Navbar() {
                 <NavLink href="#">Calendar</NavLink>
                 <LoginButton />
               </div>
+            </div>
+            <div className="ml-auto flex items-end justify-end gap-4">
+              {isLoggedIn &&
+                (session.data?.user.image ? (
+                  <Image
+                    alt="user's profile image"
+                    src={session.data.user.image}
+                    width={40}
+                    height={40}
+                  />
+                ) : (
+                  <FontAwesomeIcon
+                    icon={faUser}
+                    className="mb-3 ml-auto text-white"
+                  />
+                ))}
+              <button className="mb-2 ml-auto text-white">Account</button>
             </div>
           </div>
         </div>
